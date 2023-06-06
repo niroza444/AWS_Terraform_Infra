35b5f24ccab991a1665344bdf700374f119454e5
niroza444 <niroza444@gmail.com>
2023-06-06 14:39:12 -0400

    vars changes

--- vars.tf
+++ vars.tf
@@ -1,24 +1,98 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+variable AWS_REGION {
+  default = "us-east-2"
+}
 
-  name = var.VPC_NAME
-  cidr = var.VpcCIDR
-
-  azs             = [var.Zone1, var.Zone2, var.Zone3]
-  private_subnets = [var.PrivSub1CIDR, var.PrivSub2CIDR, var.PrivSub3CIDR]
-  public_subnets  = [var.PubSub1CIDR, var.PubSub2CIDR, var.PubSub3CIDR]
-
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
-    Terraform   = "true"
-    Environment = "Prod"
-  }
-
-  vpc_tags = {
-    Name = var.VPC_NAME
+variable AMIS {
+  type = map
+  default = {
+    us-east-2  = "ami-07efac79022b86107"
+    us-east-1  = "ami-06397100adf427136"
+    ap-south-1 = "ami-009110a2bf8d7dd0a"
   }
 }
+
+variable PRIV_KEY_PATH {
+  default = "vprofilekey"
+}
+
+variable PUB_KEY_PATH {
+  default = "vprofilekey.pub"
+}
+
+variable USERNAME {
+  default = "ubuntu"
+}
+
+variable MYIP {
+  default = "183.74.39.2/32"
+}
+
+variable rmquser {
+  default = "rabbit"
+}
+
+variable rmqpass {
+  default = "Gre@tGu!f123_"
+}
+
+variable dbuser {
+  default = "admin"
+}
+
+variable dbpass {
+  default = "admin123"
+}
+
+variable dbname {
+  default = "accounts"
+}
+
+variable instance_count {
+  default = "1"
+}
+
+variable VPC_NAME {
+  default = "vprofile-VPC"
+}
+
+variable Zone1 {
+  default = "us-east-2a"
+}
+
+variable Zone2 {
+  default = "us-east-2b"
+}
+
+variable Zone3 {
+  default = "us-east-2c"
+}
+
+variable VpcCIDR {
+  default = "172.28.0.0/16"
+}
+
+
+variable PubSub1CIDR {
+  default = "172.28.1.0/24"
+}
+
+variable PubSub2CIDR {
+  default = "172.28.2.0/24"
+}
+
+variable PubSub3CIDR {
+  default = "172.28.3.0/24"
+}
+
+variable PrivSub1CIDR {
+  default = "172.28.4.0/24"
+}
+
+variable PrivSub2CIDR {
+  default = "172.28.5.0/24"
+}
+
+variable PrivSub3CIDR {
+  default = "172.28..0/24"
+}
+
